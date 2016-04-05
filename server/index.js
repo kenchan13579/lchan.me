50590b5f0f31549b7a1c8a7040ff9ef3d7e58dce
Leung Wai Chan <kenchan13579@hotmail.com>
2016-04-04 22:50:49 -0700

    basic server setup

--- server/index.js
+++ server/index.js
@@ -2,15 +2,27 @@
 let process = require("process");
 let express = require("express");
 let config = require("./config");
-
+let morgan = require("morgan"),
+	serveStatic = require("serve-static"),
+	cookieParser = require("cookie-parser"),
+	cookieSession = require('cookie-session'),
+	bodyParser = require("body-parser");
 let app  = express();
+app.set("view engine","jade");
+
+app.use(cookieSession(config.SESSION_OPTIONS));
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());€
+app.use(cookieParser());
+app.use(serveStatic(__dirname + "/public/dist"))
 
 app.get("/", (req, res) => {
 	res.end("developing");
 });
 
+let PORT = process.argv[process.argv.length -1 ] || config.PORT || 80;
 
-let PORT = process.argv[2] || config.PORT || 80;
 app.listen(PORT, () => {
 	console.log("Listening on port %s", PORT);
 });
